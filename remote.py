5e3c62809521e6ac0328a6912ad92cd59c429182
Bana Abraham <bana.abraham@gmail.com>
2017-08-16 13:21:36 +0700

    Update remote.py

--- remote.py
+++ remote.py
@@ -25,6 +25,7 @@ def print_str(s):
     
 remote_computer = remote_run("192.168.43.44",4444)
 
+#print "hello" on local computer and "world" on remote computer 
 t_1 = threading.Thread(target=remote_computer.send_command, args=(script,))
 t_2 = threading.Thread(target=print_str, args=("hello",))
 t_1.start()
