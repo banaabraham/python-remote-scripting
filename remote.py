12d278e37ed6825679a4b83fd0de2c9fc4912680
Bana Abraham <bana.abraham@gmail.com>
2017-08-16 13:16:53 +0700

    Update remote.py

--- remote.py
+++ remote.py
@@ -1,24 +1,33 @@
-from netcat import *
+from netcat import netcat
+import threading
 
 host = '192.168.43.44'
 port = 4444
 
-remote_computer_python_script = """
-for _ in range(5):
-    print ("hello world from remote PC")
+script = """
+print("hello")
 """
+class remote_run:
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+    def send_command(self,script):
+        self.script = script
+        self.command = ("echo '%s' > run.py;python3 run.py" %(script))
+        self.result = netcat(self.host,self.port,self.command)
+        dec = [i.decode().replace("[","").replace("]","").split(",") for i in self.result]
+        res = [i for  i in dec[0]]
+        if len(res)==1:
+            res = res[0]
+        print (res+"\r") 
+def print_str(s):
+    print (s)
+    
+remote_computer = remote_run("192.168.43.44",4444)
 
-command =("echo '%s' > run.py;python3 run.py" %(remote_computer_python_script))
-
-s = netcat(host,port,command)
-
-def decoded(s):
-    dec = [i.decode().replace("[","").replace("]","").split(",") for i in s]
-    res = [i for  i in dec[0]]
-    if len(res)==1:
-        res = res[0]
-    return res
-
-c= decoded(s)    
-print(c)
-print("hello world from local PC")
+t_1 = threading.Thread(target=remote_computer.send_command, args=(script,))
+t_2 = threading.Thread(target=print_str, args=("world \r",))
+t_1.start()
+t_2.start()
+t_1.join()
+t_2.join()
